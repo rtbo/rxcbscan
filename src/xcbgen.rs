2430f46d1c8be76cb076fc159463d00ea2f0ca68
Remi Thebault <remi.thebault@gmail.com>
2021-08-18 13:50:23 +0200

    custom errors

--- src/xcbgen.rs
+++ src/xcbgen.rs
@@ -2,11 +2,12 @@ use crate::ffi::FfiXcbEmit;
 use crate::naming::Naming;
 use crate::output::Output;
 use crate::rust::RustXcbEmit;
+use quick_xml::events::attributes::Attributes;
 use quick_xml::events::Event as XmlEv;
 use quick_xml::Reader as XmlReader;
 use std::io::{self, BufRead};
 use std::path::Path;
-use std::str;
+use std::str::{self, Utf8Error};
 // pub struct TypAnnot {
 //     name: String,
 //     borrow: bool,
@@ -29,6 +30,38 @@ use std::str;
 // }
 
 #[derive(Debug)]
+pub enum XcbGenError {
+    IO(io::Error),
+    Xml(quick_xml::Error),
+    Utf8(Utf8Error),
+    Parse(String),
+}
+
+impl From<io::Error> for XcbGenError {
+    fn from(err: io::Error) -> Self {
+        XcbGenError::IO(err)
+    }
+}
+
+impl From<Utf8Error> for XcbGenError {
+    fn from(err: Utf8Error) -> Self {
+        XcbGenError::Utf8(err)
+    }
+}
+
+impl From<quick_xml::Error> for XcbGenError {
+    fn from(err: quick_xml::Error) -> Self {
+        match err {
+            quick_xml::Error::Io(e) => XcbGenError::IO(e),
+            quick_xml::Error::Utf8(e) => XcbGenError::Utf8(e),
+            e => XcbGenError::Xml(e),
+        }
+    }
+}
+
+pub type XcbGenResult<T> = Result<T, XcbGenError>;
+
+#[derive(Debug)]
 pub struct XcbGen {
     ffi: FfiXcbEmit,
     rust: RustXcbEmit,
@@ -48,7 +81,7 @@ impl XcbGen {
         }
     }
 
-    pub fn xcb_gen(mut self, xml_file: &Path) -> io::Result<()> {
+    pub fn xcb_gen(mut self, xml_file: &Path) -> XcbGenResult<()> {
         let xml = XmlReader::from_file(xml_file).unwrap();
 
         let mut parser = XcbParser {
@@ -57,14 +90,14 @@ impl XcbGen {
         };
 
         let mut imports = Vec::new();
-        let mut fst: Option<Event> = None;
+        let mut fst: Option<XcbGenResult<Event>> = None;
 
         for e in &mut parser {
-            match e {
+            match e? {
                 Event::Ignore => {}
                 Event::Import(imp) => imports.push(imp),
-                _ => {
-                    fst = Some(e);
+                ev => {
+                    fst = Some(Ok(ev));
                     break;
                 }
             }
@@ -74,8 +107,13 @@ impl XcbGen {
         self.rust.prologue(&imports)?;
 
         for ev in fst.into_iter().chain(&mut parser) {
-            self.ffi.event(&ev)?;
-            self.rust.event(&ev)?;
+            match ev {
+                Ok(ev) => {
+                    self.ffi.event(&ev)?;
+                    self.rust.event(&ev)?;
+                }
+                Err(ev) => Err(ev)?,
+            }
         }
 
         self.ffi.epilogue()?;
@@ -85,18 +123,6 @@ impl XcbGen {
     }
 }
 
-// fn find_attr<'a>(attrs: &'a Vec<OwnedAttribute>, name: &str) -> Option<&'a str> {
-//     return attrs
-//         .iter()
-//         .filter_map(|a| {
-//             if a.name.local_name == name {
-//                 Some(a.value.as_str())
-//             } else {
-//                 None
-//             }
-//         })
-//         .next();
-// }
 #[derive(Debug)]
 pub enum Event {
     Import(String),
@@ -110,56 +136,80 @@ struct XcbParser<B: BufRead> {
 }
 
 impl<B: BufRead> XcbParser<B> {
-    fn parse_import(&mut self) -> Option<String> {
-        let mut s = None;
-        loop {
-            match self.xml.read_event(&mut self.buf) {
-                Ok(XmlEv::Text(e)) => {
-                    s = Some(e.unescape_and_decode(&self.xml).unwrap());
-                }
-                Ok(XmlEv::End(ref e)) => match e.name() {
-                    b"import" => break,
-                    _ => {
-                        s = None;
-                        break;
-                    }
-                },
-                Err(_) => {
-                    s = None;
-                    break;
-                }
-                _ => {}
-            }
+    fn expect_text(&mut self) -> XcbGenResult<String> {
+        match self.xml.read_event(&mut self.buf) {
+            Ok(XmlEv::Text(e)) => Ok(e.unescape_and_decode(&self.xml)?),
+            Ok(ev) => Err(XcbGenError::Parse(format!("expected text, found {:?}", ev))),
+            Err(e) => Err(e)?,
         }
-        s
+    }
+
+    fn expect_close_tag(&mut self, tag: &[u8]) -> XcbGenResult<()> {
+        match self.xml.read_event(&mut self.buf) {
+            Ok(XmlEv::End(e)) => {
+                if e.name() == tag {
+                    Ok(())
+                } else {
+                    Err(XcbGenError::Parse(format!(
+                        "expected </{}>, got </{}>",
+                        str::from_utf8(tag).unwrap(),
+                        str::from_utf8(e.name())?
+                    )))
+                }
+            }
+            Ok(ev) => Err(XcbGenError::Parse(format!(
+                "expected </{}>, found {:?}",
+                str::from_utf8(tag).unwrap(),
+                ev
+            ))),
+            Err(e) => Err(e)?,
+        }
+    }
+
+    fn parse_import(&mut self) -> XcbGenResult<String> {
+        let imp = self.expect_text()?;
+        self.expect_close_tag(b"import")?;
+        Ok(imp)
     }
 }
 
 impl<B: BufRead> Iterator for &mut XcbParser<B> {
-    type Item = Event;
+    type Item = XcbGenResult<Event>;
 
     fn next(&mut self) -> Option<Self::Item> {
         self.buf.clear();
         match self.xml.read_event(&mut self.buf) {
             Ok(XmlEv::Empty(ref e) | XmlEv::Start(ref e)) => match e.name() {
-                b"import" => self.parse_import().map(|s| Event::Import(s)),
+                b"import" => Some(self.parse_import().map(|s| Event::Import(s))),
                 b"xidtype" => {
-                    let name = e
-                        .attributes()
-                        .find(|a| a.as_ref().unwrap().key == b"name")
-                        .expect("XID type without name")
-                        .unwrap();
-                    Some(Event::XidType(
-                        name.unescape_and_decode_value(&self.xml)
-                            .expect("can't escape argument"),
-                    ))
+                    let attrs = e.attributes();
+                    let typres = expect_attribute(attrs, b"name");
+                    Some(typres.map(|v| Event::XidType(v)))
                 }
-                _ => Some(Event::Ignore),
+                _ => Some(Ok(Event::Ignore)),
             },
 
             Ok(XmlEv::Eof) => None,
 
-            _ => Some(Event::Ignore),
+            _ => Some(Ok(Event::Ignore)),
         }
     }
+}
+
+fn expect_attribute(attrs: Attributes, name: &[u8]) -> XcbGenResult<String> {
+    for attr in attrs {
+        match attr {
+            Ok(attr) => {
+                if attr.key == name {
+                    let val = attr.unescaped_value()?;
+                    return Ok(str::from_utf8(&val)?.to_string());
+                }
+            }
+            Err(err) => Err(err)?,
+        }
+    }
+    Err(XcbGenError::Parse(format!(
+        "could not find expected attribute: {}",
+        str::from_utf8(name).unwrap()
+    )))
 }
