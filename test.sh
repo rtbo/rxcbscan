3c7383825a36af0bd7d71e9ffbd3633ab0e96dfa
Remi Thebault <remi.thebault@gmail.com>
2021-09-19 23:10:17 +0200

    move master -> main

--- test.sh
+++ test.sh
@@ -2,10 +2,10 @@ set -e
 
 cargo run
 
-python filter_comments.py gen/master/ffi gen/master.nocmt/ffi
-python filter_comments.py gen/master gen/master.nocmt
+python filter_comments.py gen/main/ffi gen/main.nocmt/ffi
+python filter_comments.py gen/main gen/main.nocmt
 python filter_comments.py gen/current/ffi gen/current.nocmt/ffi
 python filter_comments.py gen/current gen/current.nocmt
 
-python test_diff.py gen/current.nocmt/ffi gen/master.nocmt/ffi
-python test_diff.py gen/current.nocmt gen/master.nocmt
+python test_diff.py gen/current.nocmt/ffi gen/main.nocmt/ffi
+python test_diff.py gen/current.nocmt gen/main.nocmt
